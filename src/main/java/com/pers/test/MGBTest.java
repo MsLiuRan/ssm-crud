4b1d8f38fa43a0cae1198717a9b5553248afaab7
LiuRan <MsLiuRan@outlook.com>
2020-07-17 17:30:38 +0800

    框架搭建

---
+++ src/main/java/com/pers/test/MGBTest.java
@@ -0,0 +1,33 @@
+package com.pers.test;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName MGBTest
+ * @Description //TODO
+ * @Author LiuRan
+ * @Date 2020/7/17 17:01
+ * @Version 1.0
+ */
+public class MGBTest {
+    public static void main(String[] args) throws Exception {
+        System.out.println("我的中国");
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("mbg.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
